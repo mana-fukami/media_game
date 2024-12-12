f2299c1ba3e9564da03b319779dd4db422ddcb03
lan031025 <yuefenglan2017@gmail.com>
2024-12-12 17:25:03 +0900

    View2でModelのgetメソッドを使うように変更を加えた

--- グループ演習/TypingGame/View2.java
+++ グループ演習/TypingGame/View2.java
@@ -15,14 +15,15 @@ class GameView extends JFrame implements Observer{
     protected JTextField inputField;   // 入力場所←寿司打みたいに真ん中に置きたい
     protected JButton endButton;       // 終了ボタン
 
-    public GameView() {
+    public GameView(Model model) {
         setTitle("ゲーム名"); 
         setSize(1000, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
         // NORTHに問題文←具体的な配置はまた相談して決めたい
-        sentenceLabel = new JLabel("問題文", SwingConstants.CENTER);
+        // ModelでgetSentenceメソッドを作る？
+        sentenceLabel = new JLabel(""+model.getSentence(), SwingConstants.CENTER);
         sentenceLabel.setFont(new Font("Arial", Font.BOLD, 20));
         add(sentenceLabel, BorderLayout.NORTH);
 
@@ -32,11 +33,12 @@ class GameView extends JFrame implements Observer{
         add(inputField, BorderLayout.CENTER);
 
         // SOUTHにスコア、時間、終了ボタン
+        // Modelのgetメソッドを使うように変更を加えた
         JPanel bottomPanel = new JPanel(new GridLayout(1, 5));
-        scoreLabel = new JLabel("スコア: 0", SwingConstants.CENTER);
-        corLabel = new JLabel("正解: 0個", SwingConstants.CENTER);
-        missLabel = new JLabel("ミス: 0個", SwingConstants.CENTER);
-        timeLabel = new JLabel("残り時間: 60秒", SwingConstants.CENTER);
+        scoreLabel = new JLabel("スコア: "+model.getPoints(), SwingConstants.CENTER);
+        corLabel = new JLabel("正解: "+model.getCorrect()+"個", SwingConstants.CENTER);
+        missLabel = new JLabel("ミス: "+model.getMiss()+"個", SwingConstants.CENTER);
+        timeLabel = new JLabel("残り時間: "+model.getTime()+"秒", SwingConstants.CENTER);
         endButton = new JButton("ゲームを終了");
         bottomPanel.add(scoreLabel);
         bottomPanel.add(corLabel);
@@ -45,23 +47,7 @@ class GameView extends JFrame implements Observer{
         bottomPanel.add(endButton);
         add(bottomPanel, BorderLayout.SOUTH);
     }
-    
-    
-    public void setSentence(String sentence) {
-        sentenceLabel.setText(sentence);
-    }
-    public void setScore(int score) {
-        scoreLabel.setText("スコア: " + score);
-    }
-    public void setTime(int time) {
-        timeLabel.setText("残り時間: " + time + "秒");
-    }
-    public void setCorrect(int correct) {
-        corLabel.setText("正解: " + correct + "個");
-    }
-    public void setMiss(int miss) {
-        missLabel.setText("ミス: " + miss + "個");
-    }
+    // View内のsetメソッドらを消した
     public JTextField getInputField() {
         return inputField;
     }
