d8b3d2efa9d269adf41a3b79c1e69998436b644d
Fukami Mana <f2210543@gl.cc.uec.ac.jp>
2024-12-13 13:17:38 +0900

    微調整

--- グループ演習/TypingGame/Main.java
+++ グループ演習/TypingGame/Main.java
@@ -14,17 +14,17 @@ public class Main {
 		//待機ビューの作成
 		StayView stayview=new StayView();
 		//stayviewに対応するコントローラーの作成
-		Controller1 staycont=new Controller1();
+		Controller1 staycont=new Controller1(stayview,model);
 		
 		//ゲームビューの作成
-		GameView gameview=new GameView();
+		GameView gameview=new GameView(model);
 		//gameviewに対応するコントローラーの作成
-		Controller2 gamecont=new Controller2();
+		Controller2 gamecont=new Controller2(gameview,model);
 		
 		//結果ビューの作成
 		ResultView resultview=new ResultView();
 		//resultviewに対応するコントローラーの作成
-		Controller3 resultcont=new Controller3();
+		Controller3 resultcont=new Controller3(resultview,model);
 		switch(flag) {
 		case 0:
 			stayview.setVisible(true);
