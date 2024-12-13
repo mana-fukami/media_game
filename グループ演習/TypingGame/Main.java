851a595ff0a00eabc697dcbf498f769bf64b5b27
Fukami Mana <f2210543@gl.cc.uec.ac.jp>
2024-12-13 13:37:36 +0900

    待機画面はOK

--- グループ演習/TypingGame/Main.java
+++ グループ演習/TypingGame/Main.java
@@ -1,9 +1,17 @@
 package TypingGame;
 
 public class Main {
+	public static StayView stayview;
+	public static GameView gameview;
+	public static ResultView resultview;
+	
 	public static int flag=0;//0:待機,1:ゲーム,2:結果
 	//-----setter-----
-	public void setFlag(int num) {flag=num;}
+	public void setFlag(int num) {
+		flag=num;
+		main(null);
+		//System.out.println(flag);
+	}
 	//-----getter-----
 	public int getFlag() {return flag;}
 	//-----メイン関数-----
@@ -12,17 +20,17 @@ public class Main {
 		Model model=new Model();
 		
 		//待機ビューの作成
-		StayView stayview=new StayView();
+		/*StayView*/ stayview=new StayView();
 		//stayviewに対応するコントローラーの作成
 		Controller1 staycont=new Controller1(stayview,model);
 		
 		//ゲームビューの作成
-		GameView gameview=new GameView(model);
+		/*GameView*/ gameview=new GameView(model);
 		//gameviewに対応するコントローラーの作成
 		Controller2 gamecont=new Controller2(gameview,model);
 		
 		//結果ビューの作成
-		ResultView resultview=new ResultView();
+		/*ResultView*/ resultview=new ResultView();
 		//resultviewに対応するコントローラーの作成
 		Controller3 resultcont=new Controller3(resultview,model);
 		switch(flag) {
@@ -30,9 +38,11 @@ public class Main {
 			stayview.setVisible(true);
 			break;
 		case 1:
+			stayview.setVisible(false);
 			gameview.setVisible(true);
 			break;
 		case 2:
+			gameview.setVisible(false);
 			resultview.setVisible(true);
 			break;
 		}
